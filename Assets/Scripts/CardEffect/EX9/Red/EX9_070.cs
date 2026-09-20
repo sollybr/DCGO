b8ebc35c203da1db7a6ff0f9088bd2bbb689f7ae
Solly <7585871+sollybr@users.noreply.github.com>
2026-09-20 04:21:04 -0300

    Update EX9 Option Effects with proper flag

--- Assets/Scripts/CardEffect/EX9/Red/EX9_070.cs
+++ Assets/Scripts/CardEffect/EX9/Red/EX9_070.cs
@@ -52,6 +52,7 @@ namespace DCGO.CardEffects.EX9
                 ActivateClass activateClass = new ActivateClass();
                 activateClass.SetUpICardEffect("Draw 1, then place in battle area", CanUseCondition, card);
                 activateClass.SetUpActivateClass(null, hashtable => SharedActivateCoroutine(hashtable, activateClass), -1, false, EffectDiscription());
+                activateClass.SetIsOptionEffect(true);
                 cardEffects.Add(activateClass);
 
                 string EffectDiscription()
