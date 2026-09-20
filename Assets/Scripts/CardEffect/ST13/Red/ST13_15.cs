ab829baafb7c0a47f41a2a4f65cdd6481f5c9ac0
Solly <7585871+sollybr@users.noreply.github.com>
2026-09-20 04:22:39 -0300

    Update ST13 Option Effects with proper flag

--- Assets/Scripts/CardEffect/ST13/Red/ST13_15.cs
+++ Assets/Scripts/CardEffect/ST13/Red/ST13_15.cs
@@ -46,6 +46,7 @@ public class ST13_15 : CEntity_Effect
             ActivateClass activateClass = new ActivateClass();
             activateClass.SetUpICardEffect(card.BaseENGCardNameFromEntity, CanUseCondition, card);
             activateClass.SetUpActivateClass(null, ActivateCoroutine, -1, false, EffectDiscription());
+            activateClass.SetIsOptionEffect(true);
             cardEffects.Add(activateClass);
 
             string EffectDiscription()
