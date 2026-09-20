735e93f9a5e8365dd9f8d7fcd87ac3f661103659
Solly <7585871+sollybr@users.noreply.github.com>
2026-09-20 04:23:08 -0300

    Update ST18 Option Effects with proper flag

--- Assets/Scripts/CardEffect/ST18/Green/ST18_15.cs
+++ Assets/Scripts/CardEffect/ST18/Green/ST18_15.cs
@@ -16,6 +16,7 @@ namespace DCGO.CardEffects.ST18
                 ActivateClass activateClass = new ActivateClass();
                 activateClass.SetUpICardEffect(card.BaseENGCardNameFromEntity, CanUseCondition, card);
                 activateClass.SetUpActivateClass(null, ActivateCoroutine, -1, false, EffectDescription());
+                activateClass.SetIsOptionEffect(true);
                 cardEffects.Add(activateClass);
 
                 string EffectDescription()
