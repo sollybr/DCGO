ded5e4daa906cabefb5d3ad2bd373bc909a43982
Solly <7585871+sollybr@users.noreply.github.com>
2026-09-20 04:20:23 -0300

    Update EX2 Option Effects with proper flag

--- Assets/Scripts/CardEffect/EX2/Blue/EX2_069.cs
+++ Assets/Scripts/CardEffect/EX2/Blue/EX2_069.cs
@@ -45,6 +45,7 @@ namespace DCGO.CardEffects.EX2
                 ActivateClass activateClass = new ActivateClass();
                 activateClass.SetUpICardEffect(card.BaseENGCardNameFromEntity, CanUseCondition, card);
                 activateClass.SetUpActivateClass(null, ActivateCoroutine, -1, false, EffectDiscription());
+                activateClass.SetIsOptionEffect(true);
                 cardEffects.Add(activateClass);
 
                 string EffectDiscription()
--- Assets/Scripts/CardEffect/EX2/White/EX2_072.cs
+++ Assets/Scripts/CardEffect/EX2/White/EX2_072.cs
@@ -39,6 +39,7 @@ namespace DCGO.CardEffects.EX2
                 ActivateClass activateClass = new ActivateClass();
                 activateClass.SetUpICardEffect(card.BaseENGCardNameFromEntity, CanUseCondition, card);
                 activateClass.SetUpActivateClass(null, ActivateCoroutine, -1, false, EffectDescription());
+                activateClass.SetIsOptionEffect(true);
                 cardEffects.Add(activateClass);
 
                 string EffectDescription()
