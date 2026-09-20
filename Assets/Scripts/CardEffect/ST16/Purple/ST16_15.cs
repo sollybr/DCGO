01129cb60631835061f96e054780fa188bb58c1f
Solly <7585871+sollybr@users.noreply.github.com>
2026-09-20 04:22:53 -0300

    Update ST16 Option Effects with proper flag

--- Assets/Scripts/CardEffect/ST16/Purple/ST16_15.cs
+++ Assets/Scripts/CardEffect/ST16/Purple/ST16_15.cs
@@ -45,6 +45,7 @@ public class ST16_15 : CEntity_Effect
             ActivateClass activateClass = new ActivateClass();
             activateClass.SetUpICardEffect(card.BaseENGCardNameFromEntity, CanUseCondition, card);
             activateClass.SetUpActivateClass(null, ActivateCoroutine, -1, false, EffectDiscription());
+            activateClass.SetIsOptionEffect(true);
             cardEffects.Add(activateClass);
 
             string EffectDiscription()
@@ -138,6 +139,7 @@ public class ST16_15 : CEntity_Effect
                             ActivateClass activateClass1 = new ActivateClass();
                             activateClass1.SetUpICardEffect("Play this card from trash", CanUseCondition1, selectedPermanent.TopCard);
                             activateClass1.SetUpActivateClass(CanActivateCondition1, ActivateCoroutine1, -1, false, EffectDiscription1());
+                            activateClass1.SetIsOptionEffect(true);
                             activateClass1.SetEffectSourcePermanent(selectedPermanent);
                             CardEffectCommons.AddEffectToPermanent(targetPermanent: selectedPermanent, effectDuration: EffectDuration.UntilOpponentTurnEnd, card: card, cardEffect: activateClass1, timing: EffectTiming.OnDestroyedAnyone);
 
