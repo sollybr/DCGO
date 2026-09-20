bdfdaabe78578c5f66984f891080a058692812ba
Solly <7585871+sollybr@users.noreply.github.com>
2026-09-20 04:21:49 -0300

    Update ST2 Option Effects with proper flag

--- Assets/Scripts/CardEffect/ST2/Blue/ST2_16.cs
+++ Assets/Scripts/CardEffect/ST2/Blue/ST2_16.cs
@@ -18,6 +18,7 @@ public class ST2_16 : CEntity_Effect
             ActivateClass activateClass = new ActivateClass();
             activateClass.SetUpICardEffect(card.BaseENGCardNameFromEntity, CanUseCondition, card);
             activateClass.SetUpActivateClass(null, ActivateCoroutine, -1, false, EffectDiscription());
+            activateClass.SetIsOptionEffect(true);
             cardEffects.Add(activateClass);
 
             string EffectDiscription()
