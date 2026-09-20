4bb627a867549e17dad24927a57e6d019de14d58
Solly <7585871+sollybr@users.noreply.github.com>
2026-09-20 04:22:48 -0300

    Update ST15 Option Effects with proper flag

--- Assets/Scripts/CardEffect/ST15/Black/ST15_15.cs
+++ Assets/Scripts/CardEffect/ST15/Black/ST15_15.cs
@@ -45,6 +45,7 @@ public class ST15_15 : CEntity_Effect
             ActivateClass activateClass = new ActivateClass();
             activateClass.SetUpICardEffect(card.BaseENGCardNameFromEntity, CanUseCondition, card);
             activateClass.SetUpActivateClass(null, ActivateCoroutine, -1, false, EffectDiscription());
+            activateClass.SetIsOptionEffect(true);
             cardEffects.Add(activateClass);
 
             string EffectDiscription()
--- Assets/Scripts/CardEffect/ST15/Black/ST15_16.cs
+++ Assets/Scripts/CardEffect/ST15/Black/ST15_16.cs
@@ -17,6 +17,7 @@ public class ST15_16 : CEntity_Effect
             ActivateClass activateClass = new ActivateClass();
             activateClass.SetUpICardEffect(card.BaseENGCardNameFromEntity, CanUseCondition, card);
             activateClass.SetUpActivateClass(null, ActivateCoroutine, -1, false, EffectDiscription());
+            activateClass.SetIsOptionEffect(true);
             cardEffects.Add(activateClass);
 
             string EffectDiscription()
@@ -102,6 +103,7 @@ public class ST15_16 : CEntity_Effect
                         ActivateClass activateClass1 = new ActivateClass();
                         activateClass1.SetUpICardEffect("Attack with this Digimon", CanUseCondition1, selectedPermanent.TopCard);
                         activateClass1.SetUpActivateClass(CanActivateCondition1, ActivateCoroutine1, -1, false, EffectDiscription1());
+                        activateClass1.SetIsOptionEffect(true);
                         activateClass1.SetEffectSourcePermanent(selectedPermanent);
                         selectedPermanent.UntilOwnerTurnEndEffects.Add(GetCardEffect);
 
